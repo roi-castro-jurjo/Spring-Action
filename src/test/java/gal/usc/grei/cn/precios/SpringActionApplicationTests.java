acb017119c726610306e1d37377b23b9e0357d95
Roi Castro <roi.castro.jurjo@rai.usc.es>
2023-12-16 18:40:46 +0100

    Initial commit

---
+++ src/test/java/gal/usc/grei/cn/precios/SpringActionApplicationTests.java
@@ -0,0 +1,13 @@
+package gal.usc.grei.cn.precios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
